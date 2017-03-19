3b703cd6556060d3c0cbbb64cae86de1e917818c
Vipul Marwah <vipul_marwah777@yahoo.com>
2017-03-19 15:43:26 +0800

    committing kausik.java

---
+++ src/main/java/com/mkyong/controller/kausik.java
@@ -0,0 +1,12 @@
+package com.mkyong.controller;
+public class kausik {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        
+	System.out.println("Hello, Harshad with me today in the DevOps tutorial by Kausik the great");
+    }
+
+}
+
+
